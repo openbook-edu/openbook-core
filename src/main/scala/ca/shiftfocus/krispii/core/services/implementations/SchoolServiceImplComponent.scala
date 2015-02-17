4ff4426f306e950199990082d7eee880a9d64753
Andrei Ostapenco <ao@shiftfocus.ca>
2015-02-17 12:25:36 -0500

    SchoolServiceComponent merge confilicts

--- src/main/scala/ca/shiftfocus/krispii/core/services/implementations/SchoolServiceImplComponent.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/services/implementations/SchoolServiceImplComponent.scala
@@ -223,7 +223,7 @@ trait SchoolServiceImplComponent extends SchoolServiceComponent {
      * @param courseId the [[UUID]] of the [[Course]] to list parts for
      * @return an [[IndexedSeq]] of [[Part]].
      */
-<<<<<<< HEAD
+
 //    override def listEnabledParts(projectId: UUID, classId: UUID): Future[IndexedSeq[Part]] = {
 //      val fProject = projectRepository.find(projectId).map(_.get)
 //      val fSection = classRepository.find(classId).map(_.get)
@@ -244,15 +244,6 @@ trait SchoolServiceImplComponent extends SchoolServiceComponent {
         enabledParts <- {
           Future successful project.parts.filter(_.enabled)
         }
-=======
-    override def listEnabledParts(projectId: UUID, courseId: UUID): Future[IndexedSeq[Part]] = {
-      val fProject = projectRepository.find(projectId).map(_.get)
-      val fCourse = courseRepository.find(courseId).map(_.get)
-      for {
-        project <- fProject
-        course <- fCourse
-        enabledParts <- partRepository.listEnabled(project, course)
->>>>>>> 8503d0e34feac86f3642ac8a881f179e0d270091
       }
       yield enabledParts
     }.recover {
@@ -421,17 +412,5 @@ trait SchoolServiceImplComponent extends SchoolServiceComponent {
 //      }
 //      yield isEnabled
 //    }
-
-<<<<<<< HEAD
-=======
-    override def isPartEnabledForCourse(partId: UUID, courseId: UUID): Future[Boolean] = {
-      for {
-        part <- partRepository.find(partId).map(_.get)
-        course <- courseRepository.find(courseId).map(_.get)
-        isEnabled <- partRepository.isEnabled(part, course)
-      }
-      yield isEnabled
-    }
->>>>>>> 8503d0e34feac86f3642ac8a881f179e0d270091
   }
 }

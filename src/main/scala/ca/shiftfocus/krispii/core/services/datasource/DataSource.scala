2bf717814f9afe43dbddea7a3dbd62a123b4e0b0
Chris Kahn <ck@shiftfocus.ca>
2015-03-03 16:46:58 -0500

    Cleaned up DataSource class

--- src/main/scala/ca/shiftfocus/krispii/core/services/datasource/DataSource.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/services/datasource/DataSource.scala
@@ -1,31 +1,13 @@
 package ca.shiftfocus.krispii.core.services.datasource
 
-import ca.shiftfocus.krispii.core.error.ErrorUnion
-import ca.shiftfocus.krispii.core.lib.FutureMonad
 import com.github.mauricio.async.db.Configuration
 import com.github.mauricio.async.db.Connection
 import com.github.mauricio.async.db.postgresql.pool.PostgreSQLConnectionFactory
 import com.github.mauricio.async.db.pool.{PoolConfiguration, ConnectionPool}
-import com.github.mauricio.async.db.util.ExecutorServiceUtils.CachedExecutionContext
-
 import com.typesafe.config._
-import ca.shiftfocus.krispii.core.fail._
-import scala.concurrent.Future
 import scalaz.{\/, -\/, \/-}
 
-case class OutOfDateException(msg: String) extends Exception
-
 trait DB {
-  /**
-   * Takes a function that returns a future, and runs it inside a database
-   * transaction.
-   */
-  def transactional[A](f : Connection => Future[A]) = {
-    pool.inTransaction(f).recover {
-      case exception => throw exception
-    }
-  }
-
   val dbconfig: Configuration
   def pool: Connection
 }

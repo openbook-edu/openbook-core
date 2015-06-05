ce3e71966af3ec494dc1bf8235f62f1435eede66
Chris Kahn <ck@shiftfocus.ca>
2015-06-05 10:35:44 -0400

    Adding UUID reads/writes

--- src/main/scala/ca/shiftfocus/krispii/core/lib/UUIDOps.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/lib/UUIDOps.scala
@@ -2,6 +2,23 @@ package ca.shiftfocus.krispii.core.lib
 
 import java.util.UUID
 
-//implicit class RichUuid(val self: UUID) extends AnyVal {
-//  //def equals
-//}
+import play.api.libs.json._
+
+trait UUIDOps {
+  implicit val reads =  new Reads[UUID] {
+    def reads(json: JsValue) = {
+      try {
+        JsSuccess(UUID.fromString(json.as[String]))
+      } catch {
+        case exp: IllegalArgumentException => JsError(s"The string '${json.as[String]}' is not a valid UUID")
+        case exp: Throwable => throw exp
+      }
+    }
+  }
+
+  implicit val writes = new Writes[UUID] {
+    def writes(uuid: UUID) = {
+      JsString(uuid.toString)
+    }
+  }
+}

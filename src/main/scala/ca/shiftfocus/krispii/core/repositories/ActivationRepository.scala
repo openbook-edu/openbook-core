69f7e08611b80a6ef5b384fa650aa70face703d3
Rafael Yanez <rafael@krispii.com>
2016-02-15 12:28:44 -0500

    ActivationRepository trait and postgres implementation

--- src/main/scala/ca/shiftfocus/krispii/core/repositories/ActivationRepository.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/repositories/ActivationRepository.scala
@@ -2,22 +2,23 @@ package ca.shiftfocus.krispii.core.repositories
 
 import java.util.UUID
 
-import _root_.helpers.UUID
-import _root_.helpers.expect._
+import ca.shiftfocus.krispii.core.error.RepositoryError
+import ca.shiftfocus.krispii.core.lib.ScalaCachePool
 import ca.shiftfocus.krispii.core.models.UserToken
-import models.UserToken
 import com.github.mauricio.async.db.{ RowData, Connection }
 
+import scala.concurrent.Future
+import scalaz.\/
 
 /**
-  * Handles the CRUD operations related to the token of a new user when signing up.
-  * Created by ryanez on 11/02/16.
-  */
+ * Handles the CRUD operations related to the token of a new user when signing up.
+ * Created by ryanez on 11/02/16.
+ */
 trait ActivationRepository extends Repository {
   def constructor(row: RowData): UserToken
-  def find(userId: UUID): ExpectReader[Connection, UserToken]
-  def find(email: String): ExpectReader[Connection, UserToken]
-  def insert(userId: UUID, nonce: String): ExpectReader[Connection, UserToken]
-  def update(userId: UUID, nonce: String): ExpectReader[Connection, UserToken]
-  def delete(userId: UUID): ExpectReader[Connection, UserToken]
+  def find(userId: UUID)(implicit conn: Connection, cache: ScalaCachePool): Future[\/[RepositoryError.Fail, UserToken]]
+  def find(email: String)(implicit conn: Connection, cache: ScalaCachePool): Future[\/[RepositoryError.Fail, UserToken]]
+  def insert(userId: UUID, nonce: String)(implicit conn: Connection, cache: ScalaCachePool): Future[\/[RepositoryError.Fail, UserToken]]
+  def update(userId: UUID, nonce: String)(implicit conn: Connection, cache: ScalaCachePool): Future[\/[RepositoryError.Fail, UserToken]]
+  def delete(userId: UUID)(implicit conn: Connection, cache: ScalaCachePool): Future[\/[RepositoryError.Fail, UserToken]]
 }

5295286d142cd06ea596cba94e37463df25fcde9
Rafael Yanez <rafael@krispii.com>
2016-02-15 12:30:44 -0500

    updated the UserToken, needs to be finished

--- src/main/scala/ca/shiftfocus/krispii/core/models/UserToken.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/models/UserToken.scala
@@ -2,23 +2,26 @@ package ca.shiftfocus.krispii.core.models
 
 import java.util.UUID
 
-import play.api.libs.json.{Writes, Reads}
+import org.joda.time.DateTime
+import play.api.libs.json._
+import play.api.libs.json.Reads._
+import play.api.libs.functional.syntax._
 
 /**
-  * Represents the authentication token assign to a new user when signing up.
-  * Created by ryanez on 11/02/16.
-  */
-case class UserToken (
-  id: UUID,
+ * Represents the authentication token assign to a new user when signing up.
+ * Created by ryanez on 11/02/16.
+ */
+case class UserToken(
+  userId: UUID,
   token: String
-                     ) {}
+) {}
 
 object UserToken {
-  implicit val activationReads: Reads[UserToken] = (
-      (__ \ "id").read[UUID] and (__ \ "token").read[String]
-    ) (UserToken.apply _)
-
-  implicit val activationWrites: Writes[UserToken] = (
-      (__ \ "id").write[UUID] and (__ \ "token").write[String]
-    ) (unlift(UserToken.unapply()))
+  //  implicit val activationReads: Reads[UserToken] = (
+  //      (JsPath \ "id").read[UUID] and (JsPath \ "token").read[String]
+  //    ) (UserToken.apply _)
+  //
+  //  implicit val activationWrites: Writes[UserToken] = (
+  //      (JsPath \ "id").write[UUID] and (JsPath \ "token").write[String]
+  //    ) (unlift(UserToken.unapply))
 }

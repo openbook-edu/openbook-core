6eab72902e49ff178a64291fa5919255b912079c
Andrei Ostapenco <ao@shiftfocus.ca>
2017-02-21 14:34:00 -0500

    KCLASP-345 create invoice when switching between plans with the same interval

--- src/main/scala/ca/shiftfocus/krispii/core/services/PaymentServiceDefault.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/services/PaymentServiceDefault.scala
@@ -406,7 +406,18 @@ class PaymentServiceDefault(
           params.put("plan", newPlanId)
 
           val subscription: Subscription = Subscription.retrieve(subscriptionId, requestOptions)
+          val currentPlan: Plan = subscription.getPlan
+          val newPlan: Plan = Plan.retrieve(newPlanId)
           val updatedSubsctiption = subscription.update(params, requestOptions)
+
+          // If plans intervals match then we should manually create an invoice and force payment
+          if (currentPlan.getInterval == newPlan.getInterval) {
+            val customerId = subscription.getCustomer
+            val invoiceParams = new java.util.HashMap[String, Object]()
+            invoiceParams.put("customer", customerId)
+            val invoice: Invoice = Invoice.create(invoiceParams)
+            invoice.pay()
+          }
 
           \/-(updatedSubsctiption)
         }

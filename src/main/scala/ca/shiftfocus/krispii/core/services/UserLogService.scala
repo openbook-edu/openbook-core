240864b6941c38d2555ac0aa5aea02f6f49dc22c
Andrei Ostapenco <ao@shiftfocus.ca>
2017-09-18 15:07:49 -0400

    KCLASP-824 Tracking of logins

---
+++ src/main/scala/ca/shiftfocus/krispii/core/services/UserLogService.scala
@@ -0,0 +1,13 @@
+package ca.shiftfocus.krispii.core.services
+
+import java.util.UUID
+
+import ca.shiftfocus.krispii.core.error.ErrorUnion
+import ca.shiftfocus.krispii.core.models.UserLog
+
+import scala.concurrent.Future
+import scalaz.\/
+
+trait UserLogService extends Service[ErrorUnion#Fail] {
+  def create(userId: UUID, logType: String, data: Option[String]): Future[\/[ErrorUnion#Fail, UserLog]]
+}

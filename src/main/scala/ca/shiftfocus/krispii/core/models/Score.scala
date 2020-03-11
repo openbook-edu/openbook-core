e9dc07516cee87032d17b2a7294695602647c5ed
Matthias Gralle <mathias@krispii.com>
2020-03-11 12:38:55 -0400

    jsonWrites for Score

--- src/main/scala/ca/shiftfocus/krispii/core/models/Score.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/models/Score.scala
@@ -1,11 +1,11 @@
 package ca.shiftfocus.krispii.core.models
 
 import java.util.UUID
+
 import org.joda.time.DateTime
-import play.api.libs.json._
-import play.api.libs.json.Writes._
-import play.api.libs.functional.syntax._
 import play.api.libs.json.JodaWrites._
+import play.api.libs.json.Writes._
+import play.api.libs.json._
 
 case class Score(
   work_id: UUID,
@@ -14,15 +14,19 @@ case class Score(
   createdAt: DateTime = new DateTime,
   updatedAt: DateTime = new DateTime,
   grade: String = ""
-)
+) {}
 
 object Score {
-  implicit val writes: Writes[Score] = (
-    (__ \ "work_id").write[UUID] and
-    (__ \ "scorer_id").write[UUID] and
-    (__ \ "version").write[Long] and
-    (__ \ "createdAt").write[DateTime] and
-    (__ \ "updatedAt").write[DateTime] and
-    (__ \ "grade").write[String]
-  )(unlift(Score.unapply))
+  implicit val jsonWrites = new Writes[Score] {
+    def writes(score: Score): JsValue = {
+      Json.obj(
+        "work_id" -> score.work_id,
+        "scorer_id" -> score.scorer_id,
+        "version" -> score.version,
+        "createdAt" -> score.createdAt,
+        "updatedAt" -> score.updatedAt,
+        "grade" -> score.grade
+      )
+    }
+  }
 }

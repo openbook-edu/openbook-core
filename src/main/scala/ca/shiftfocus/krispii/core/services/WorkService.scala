c0deab4857aaa8878cdf21200b60de20d56fc7ae
Matthias Gralle <matthias@krispii.com>
2020-09-23 15:17:18 -0400

    Save Microsoft access and refresh tokens in Session

--- src/main/scala/ca/shiftfocus/krispii/core/services/WorkService.scala
+++ src/main/scala/ca/shiftfocus/krispii/core/services/WorkService.scala
@@ -82,4 +82,19 @@ trait WorkService extends Service[ErrorUnion#Fail] {
   ): Future[\/[ErrorUnion#Fail, Work]]
   def updateGfile(gFileId: UUID, sharedEmail: Option[Option[String]], permissionId: Option[Option[String]], revisionId: Option[Option[String]]): Future[\/[ErrorUnion#Fail, Work]]
   def deleteGfile(gFileId: UUID): Future[\/[ErrorUnion#Fail, Work]]
+
+  // ########## MICROSOFT FILES ###########################################################################################
+  def getMSfile(msFileId: UUID): Future[\/[ErrorUnion#Fail, Gfile]]
+  def createMSfile(
+    workId: UUID,
+    fileId: String,
+    mimeType: String,
+    fileType: String,
+    fileName: String,
+    embedUrl: String,
+    url: String,
+    sharedEmail: Option[String]
+  ): Future[\/[ErrorUnion#Fail, Work]]
+  def updateMSfile(msFileId: UUID, sharedEmail: Option[Option[String]], permissionId: Option[Option[String]], revisionId: Option[Option[String]]): Future[\/[ErrorUnion#Fail, Work]]
+  def deleteMSfile(msFileId: UUID): Future[\/[ErrorUnion#Fail, Work]]
 }

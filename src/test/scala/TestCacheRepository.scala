1ab4d118516c5f7f4e4973a9de9cc99e3c6af9df
Matthias Gralle <mathias@krispii.com>
2021-09-15 10:31:03 -0400

    Fixed TestCacheRepository

--- src/test/scala/TestCacheRepository.scala
+++ src/test/scala/TestCacheRepository.scala
@@ -4,12 +4,13 @@ import ca.shiftfocus.krispii.core.repositories.CacheRepository
 import org.scalamock.scalatest.MockFactory
 
 import scalacache.Cache
+import scalacache.serialization.Codec
 
 class TestCacheRepository(
     override val scalaCacheConfig: ScalaCacheConfig
 ) extends CacheRepository(scalaCacheConfig) with MockFactory {
   def master[A]: Cache[A] = stub[Cache[A]]
-  class TestScalaCachePool[A] extends ScalaCachePool[A](master)
+  class TestScalaCachePool[A] extends ScalaCachePool[A](scalaCacheConfig)(master[A].config, codec.asInstanceOf[Codec[A]])
 
   override val cacheTask: ScalaCachePool[Task] = stub[TestScalaCachePool[Task]]
 }
